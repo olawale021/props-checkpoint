4dd3fbe503ecafcf30547197fb8e1ea0f5bc2926
olawale021 <olawalefilani@gmail.com>
2023-07-25 07:30:00 +0100

    props-checkpoint

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import PlayerList from './components/playerList';
 function App() {
   return (
     <div className="App">
-      <h2 style={{fontSize: "40px"}}>My Top 4 Football Players</h2>
+      <h2 style={{fontSize: "40px"}}>Top 4 Football Players</h2>
       <PlayerList />
     </div>
   );
